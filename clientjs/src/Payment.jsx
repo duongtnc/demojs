839985cbb42f6a7872c2fdfcc481b7578b9fc5fe
duongtnc <duongtnc@ldcc.vn>
2023-04-04 21:25:39 +0700

    format code

--- clientjs/src/Payment.jsx
+++ clientjs/src/Payment.jsx
@@ -3,29 +3,15 @@ import React from "react";
 
 function App() {
   function handleClick (){
- 
- 
-    Axios.post('http://localhost:8080/payment', {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
+    Axios.post('http://localhost:8080/payment', {  
     data: JSON.stringify({cpfcnpj: "15060655709"})
-    }).then(res =>  window.location.href = "https://www.google.com")
+    }).then(res =>  console.log(res.data))
       .catch(error => console.log(error))
-      
   }
- 
-
-  
   return (
     <div className="Payment">
       <header className="Payment-header">
-     
-       
-          <button type="submit" onClick={handleClick} >send payment</button>
-            
- 
+        <button type="submit" onClick={handleClick} >send payment</button>
       </header>
     </div>
   );
